c9be11936b746b94985194fa64a92e1cdeace6ad
zack.huang <zack.huang@watson.com.cn>
2018-04-16 13:51:37 +0800

    Initial commit

---
+++ src/main/java/com/htc/coastline/CoastlineApplication.java
@@ -0,0 +1,12 @@
+package com.htc.coastline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoastlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoastlineApplication.class, args);
+    }
+}
