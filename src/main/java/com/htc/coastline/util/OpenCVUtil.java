e4fe6a6a04bbd360e68d35b8559491d42fc73caa
hz8080 <htchz2528@gmail.com>
2018-04-24 02:20:09 +0800

    messssss

--- src/main/java/com/htc/coastline/util/OpenCVUtil.java
+++ src/main/java/com/htc/coastline/util/OpenCVUtil.java
@@ -1,8 +1,6 @@
 package com.htc.coastline.util;
 
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.Size;
+import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.springframework.util.ResourceUtils;
@@ -10,6 +8,7 @@ import org.springframework.util.ResourceUtils;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.URL;
+import java.util.Collections;
 
 public class OpenCVUtil {
     static {
@@ -18,18 +17,35 @@ public class OpenCVUtil {
 
     public static void cvCanny(){
         try {
-            File file = ResourceUtils.getFile("classpath:static/images/1/LC08_L1TP_123045_20131003_20170429_01_T1.jpg");
+            File file = ResourceUtils.getFile("classpath:static/images/1/20160922110329239.jpeg");
 
             Mat src = Imgcodecs.imread(file.getAbsolutePath(), Imgcodecs.IMREAD_GRAYSCALE);
 
             Mat dst = src.clone();
-            Imgproc.GaussianBlur(src, dst, new Size(3, 3), 0);
+//            Imgproc.GaussianBlur(src, dst, new Size(3, 3), 0);
+//            Imgproc.medianBlur(src, dst, 9);
+//            Imgcodecs.imwrite("D:\\median.jpg", dst);
+
+            Mat matGrey = new Mat();
+            MatOfInt histSize = new MatOfInt(256);
+            MatOfFloat histRange = new MatOfFloat(0, 256);
+            MatOfInt channels = new MatOfInt(0);
+            Imgproc.calcHist(Collections.singletonList(dst), channels, new Mat(), matGrey, histSize, histRange, false);
+            for (int i = 0; i < 256; i++){
+                System.out.println(String.format("bin/value[%d]:", i) + matGrey.get(i, 0)[0]);
+            }
+
+            Imgproc.threshold(dst, dst, 11478.0, 10, Imgproc.THRESH_BINARY);
 
             Imgproc.Canny(dst, dst, 40, 100);
 
-            Imgcodecs.imwrite("D:\\canny1.jpg", dst);
+            Imgcodecs.imwrite("D:\\canny.jpg", dst);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        cvCanny();
     }
 }
